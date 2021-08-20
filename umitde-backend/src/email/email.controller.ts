6dc1a5629af70cc027de43cce1cd63aeec764a01
umtdemr <umitde692@gmail.com>
2021-08-20 17:20:28 +0300

    now reqs are working

--- umitde-backend/src/email/email.controller.ts
+++ umitde-backend/src/email/email.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { EmailService } from './email.service';
 
 @Controller('email')
--- umitde-backend/src/email/email.service.ts
+++ umitde-backend/src/email/email.service.ts
@@ -3,11 +3,8 @@ import { Email } from './email.model';
 
 @Injectable()
 export class EmailService {
-  email: Email;
-
   sendEmailService(title: string, email: string, message: string) {
     const newMail = new Email(title, email, message);
-    console.log(newMail);
     return newMail;
   }
 }
