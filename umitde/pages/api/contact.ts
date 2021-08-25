1090287c644cf9063abf3638c76d860c6be20eb9
umtdemr <umitde692@gmail.com>
2021-08-25 18:15:32 +0300

    added dotenv

--- umitde/pages/api/contact.ts
+++ umitde/pages/api/contact.ts
@@ -5,15 +5,16 @@ export default function contact(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    require('dotenv').config()
     console.log(req.body);
     let nodemailer = require('nodemailer')
     const transporter = nodemailer.createTransport({
-        port: 465,
-        host: "smtp.gmail.com",
+        port: parseInt(process.env.mail_port!),
+        host: process.env.mail_host,
         auth: {
-          user: 'demo@demo.gmail',
-          pass: 'password',
+          user: process.env.mail_acc,
+          pass: process.env.mail_password,
         },
         secure: true,
-      })
+    });
 }
