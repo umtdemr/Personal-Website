3abe95210b935e5ac63d8f9d9783b0187f855cc4
umtdemr <umitde692@gmail.com>
2021-08-25 18:02:37 +0300

    base nodemailer setting

--- umitde/pages/api/contact.ts
+++ umitde/pages/api/contact.ts
@@ -1,9 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 
 
-export default function (
+export default function contact(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     console.log(req.body);
+    let nodemailer = require('nodemailer')
+    const transporter = nodemailer.createTransport({
+        port: 465,
+        host: "smtp.gmail.com",
+        auth: {
+          user: 'demo@demo.gmail',
+          pass: 'password',
+        },
+        secure: true,
+      })
 }
