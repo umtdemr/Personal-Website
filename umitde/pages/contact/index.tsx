5a8ffe99e20adfc527ca0b392ea75f19affd3305
umtdemr <umitde692@gmail.com>
2021-08-26 15:47:59 +0300

    contact refactor

--- umitde/pages/contact/index.tsx
+++ umitde/pages/contact/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React, { ChangeEvent, useRef, useState, useEffect, FormEvent } from 'react'
 
 import { ValidType, ValidInputs, MailData } from "../../types/contact.types";
+import send_email from "../../utils/contact/send_email";
 
 
 const Contact: NextPage = () => {
@@ -91,12 +92,8 @@ const Contact: NextPage = () => {
       setIsValid(isValidAll.every(item => item.isValid));
     }, [isValidAll]);
 
-    useEffect(() => {
-      if (isLoading) {
-      }
-    }, [isLoading]);
 
-    const sendMessage = (e: FormEvent) => {
+    const sendMessage = async (e: FormEvent) => {
       e.preventDefault();
       console.log("mesaj gönder");
       if (isLoading)
@@ -106,43 +103,12 @@ const Contact: NextPage = () => {
         message,
         email,
       }
-      setIsLoading(true);
-      const form_container = document.querySelector(".form_container")!;
-      const success_container = document.querySelector(".form_message .success")!;
-      const error_container = document.querySelector(".form_message .error")!;
-      fetch('/api/contact/', {
-        method: "POST",
-        headers: {
-          'Accept': 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      })
-      .then(res => res.json())
-      .then(res => {
-        form_container.classList.add("message-received");
-        console.log(res);
-          if (res.sent) {
-            setTimeout(() => {
-              success_container.classList.add("active");
-            }, 300);
-          }else {
-            setTimeout(() => {
-              error_container.classList.add("active");
-            }, 300);
-            setErrMsg(res.error);
-          }
-        })
-        .finally(() => {
-          setIsLoading(false);
-          setTimeout(() => {
-            success_container.classList.remove("active");
-            error_container.classList.remove("active");
-          }, 11500);
-          setTimeout(() => {
-            form_container.classList.remove("message-received");
-          }, 12000);
-        })
+      
+      await send_email(
+        setIsLoading,
+        setErrMsg,
+        data
+      );
     }
 
     return (
