c31b9739c94ee9b4c992f867495fff1eb2d7b09b
umtdemr <umitde692@gmail.com>
2021-08-26 16:46:51 +0300

    added nprogress

--- umitde/pages/_app.tsx
+++ umitde/pages/_app.tsx
@@ -2,9 +2,15 @@ import '../public/css/main.min.css'
 import '../public/css/custom.css'
 import '../public/plugins/fontawesome/css/all.min.css'
 import type { AppProps } from 'next/app'
+import Router from 'next/router';
 import Layout from '../components/layout/layout'
 
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
 
+NProgress.configure({ showSpinner: false });
+
+Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done());  
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
