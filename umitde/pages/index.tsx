6676a5761e315dd559a0e261960ec54c16c53248
umtdemr <umitde692@gmail.com>
2021-08-31 18:42:09 +0300

    custom anims

--- umitde/pages/index.tsx
+++ umitde/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -14,7 +15,9 @@ const Home: NextPage = () => {
             2016&apos;dan bu yana full stack web developer olma yolunda ilerliyorum. Bunun yanında hobi amaçlı mobil kodlamaya da ilgim bulunmakta.
             2019&apos;un mart ayından beri part time full stack web developer pozisyonunda çalışıyorum.
           </p>
-          <a href="#" className="purple_button">Dahasını Öğren</a>
+          <Link href="/about" passHref>
+            <a href="#" className="purple_button">Dahasını Öğren</a>
+          </Link>
         </div>
         <div className="info--right">
           <Image src="/svg/develop.svg" alt="Thats me" width="1076" height="755"/>
