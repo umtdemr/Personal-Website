36c5bcab646aebdbe6d55c97160f6375a6e7715b
umtdemr <umitde692@gmail.com>
2021-09-01 22:15:07 +0300

    started to localization

--- umitde/next.config.js
+++ umitde/next.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   reactStrictMode: true,
   distDir: 'build',
+  i18n: {
+    locales: ['en', 'tr'],
+    defaultLocale: 'tr',
+  },
   env: {
     CAPCTCHA_SITE_KEY: process.env.SITE_KEY,
     CAPCTCHA_SECET_KEY: process.env.SECRET_KEY,
