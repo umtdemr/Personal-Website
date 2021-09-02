77d82f4f8f5360b9b84ef7e9058fbdee5348454e
umtdemr <umitde692@gmail.com>
2021-09-02 21:27:08 +0300

    translate that comes from same path

--- umitde/components/translate/change-lang-btn.tsx
+++ umitde/components/translate/change-lang-btn.tsx
@@ -12,24 +12,24 @@ const ChangeLangBtn: React.FC<ChangeLangBtnTypes> = ({isMobile}) => {
     const router = useRouter();
     const { locale } = router;
     const change_locale = locale === "tr" ? "en" : "tr"
-   return (
-        <button
-            className="
-            white_button
-            with_arrow
-            animate__animated animate__fadeInRight
-            "
-            style={{flexDirection: "column"}}
-            onClick={
-                !isMobile ?
-                () => router.push("/", "/", { locale: change_locale })
-                :
-                () => router.push("/", "/", { locale: change_locale }).then(() => toggleMobileMenu())
-            }
-        >
-            {change_locale.toUpperCase()}
-        </button>
-   ) 
+    return (
+            <button
+                className="
+                white_button
+                with_arrow
+                animate__animated animate__fadeInRight
+                "
+                style={{flexDirection: "column"}}
+                onClick={
+                    !isMobile ?
+                    () => router.push(router.pathname, router.pathname, { locale: change_locale })
+                    :
+                    () => router.push(router.pathname, router.pathname, { locale: change_locale }).then(() => toggleMobileMenu())
+                }
+            >
+                {change_locale.toUpperCase()}
+            </button>
+    ) 
 }
 
 
