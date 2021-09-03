9154735fc037afac09cb061a98d412c6618e7a4a
umtdemr <umitde692@gmail.com>
2021-09-03 22:16:44 +0300

    comt

--- umitde/pages/portfolio/index.tsx
+++ umitde/pages/portfolio/index.tsx
@@ -3,6 +3,8 @@ import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
 
+import Masonry from "react-masonry-component"
+
 
 const Portfolio: NextPage = () => {
   return (
@@ -14,34 +16,88 @@ const Portfolio: NextPage = () => {
         <h2 className="portfolio_wrapper--title">Portfolyo</h2>
         <p className="portfolio_wrapper--text">Bugüne kadar yaptığım bazı işler</p>
         <div className="portfolio">
-          <div className="portfolio__item">
-            <img src="/images/portfolio/hierapi.png" alt="Hierapi" className="portfolio__item--img"/>
-            <span className="portfolio__item--title">Hierapi</span>
-            <span className="portfolio__item--end">E Ticaret</span>
-            <div className="portfolio__item--overlay"></div>
-            <a href="https://hierapi.com" target="_blank" rel="noreferrer">Projeye göz at</a>
-          </div>
-          <div className="portfolio__item">
-            <img src="/images/portfolio/hierapi.png" alt="Hierapi" className="portfolio__item--img"/>
-            <span className="portfolio__item--title">Hierapi</span>
-            <span className="portfolio__item--end">E Ticaret</span>
-            <div className="portfolio__item--overlay"></div>
-            <a href="https://hierapi.com" target="_blank" rel="noreferrer">Projeye göz at</a>
-          </div>
-          <div className="portfolio__item">
-            <img src="/images/portfolio/hierapi.png" alt="Hierapi" className="portfolio__item--img"/>
-            <span className="portfolio__item--title">Hierapi</span>
-            <span className="portfolio__item--end">E Ticaret</span>
-            <div className="portfolio__item--overlay"></div>
-            <a href="https://hierapi.com" target="_blank" rel="noreferrer">Projeye göz at</a>
-          </div>
-          <div className="portfolio__item">
-            <img src="/images/portfolio/hierapi.png" alt="Hierapi" className="portfolio__item--img"/>
-            <span className="portfolio__item--title">Hierapi</span>
-            <span className="portfolio__item--end">E Ticaret</span>
-            <div className="portfolio__item--overlay"></div>
-            <a href="https://hierapi.com" target="_blank" rel="noreferrer">Projeye göz at</a>
-          </div>
+          <Masonry
+            className="portfolio__item-s"
+          >
+            <div className="portfolio__item" style={{height: "300px"}}>
+              <img src="/images/portfolio/hierapi.png" alt="Hierapi" className="portfolio__item--img"/>
+              <div className="portfolio__item--content">
+                <div className="content_header">
+                  <span className="portfolio__item--title">Hierapi</span>
+                  <span className="portfolio__item--end">E Ticaret</span>
+                </div>
+                <div className="content_bottom">
+                  <a href="https://hierapi.com" target="_blank" rel="noreferrer">Projeye göz at</a>
+                </div>
+              </div>
+              <div className="portfolio__item--overlay" style={{backgroundColor: "#212353"}}></div>
+            </div>
+            <div className="portfolio__item" style={{height: "300px"}}>
+              <img src="/images/portfolio/hierapi.png" alt="Hierapi" className="portfolio__item--img"/>
+              <div className="portfolio__item--content">
+                <div className="content_header">
+                  <span className="portfolio__item--title">Hierapi</span>
+                  <span className="portfolio__item--end">E Ticaret</span>
+                </div>
+                <div className="content_bottom">
+                  <a href="https://hierapi.com" target="_blank" rel="noreferrer">Projeye göz at</a>
+                </div>
+              </div>
+              <div className="portfolio__item--overlay" style={{backgroundColor: "#9c69e2"}}></div>
+            </div>
+            <div className="portfolio__item" style={{height: "300px"}}>
+              <img src="/images/portfolio/hierapi.png" alt="Hierapi" className="portfolio__item--img"/>
+              <div className="portfolio__item--content">
+                <div className="content_header">
+                  <span className="portfolio__item--title">Hierapi</span>
+                  <span className="portfolio__item--end">E Ticaret</span>
+                </div>
+                <div className="content_bottom">
+                  <a href="https://hierapi.com" target="_blank" rel="noreferrer">Projeye göz at</a>
+                </div>
+              </div>
+              <div className="portfolio__item--overlay" style={{background: "linear-gradient(90deg,#777bf8,#9c69e2 200%)"}}></div>
+            </div>
+            <div className="portfolio__item" style={{height: "300px"}}>
+              <img src="/images/portfolio/hierapi.png" alt="Hierapi" className="portfolio__item--img"/>
+              <div className="portfolio__item--content">
+                <div className="content_header">
+                  <span className="portfolio__item--title">Hierapi</span>
+                  <span className="portfolio__item--end">E Ticaret</span>
+                </div>
+                <div className="content_bottom">
+                  <a href="https://hierapi.com" target="_blank" rel="noreferrer">Projeye göz at</a>
+                </div>
+              </div>
+              <div className="portfolio__item--overlay" style={{backgroundColor: "#657D39"}}></div>
+            </div>
+            <div className="portfolio__item" style={{height: "300px"}}>
+              <img src="/images/portfolio/hierapi.png" alt="Hierapi" className="portfolio__item--img"/>
+              <div className="portfolio__item--content">
+                <div className="content_header">
+                  <span className="portfolio__item--title">Hierapi</span>
+                  <span className="portfolio__item--end">E Ticaret</span>
+                </div>
+                <div className="content_bottom">
+                  <a href="https://hierapi.com" target="_blank" rel="noreferrer">Projeye göz at</a>
+                </div>
+              </div>
+              <div className="portfolio__item--overlay" style={{backgroundColor: "#C54647"}}></div>
+            </div>
+            <div className="portfolio__item" style={{height: "300px"}}>
+              <img src="/images/portfolio/hierapi.png" alt="Hierapi" className="portfolio__item--img"/>
+              <div className="portfolio__item--content">
+                <div className="content_header">
+                  <span className="portfolio__item--title">Hierapi</span>
+                  <span className="portfolio__item--end">E Ticaret</span>
+                </div>
+                <div className="content_bottom">
+                  <a href="https://hierapi.com" target="_blank" rel="noreferrer">Projeye göz at</a>
+                </div>
+              </div>
+              <div className="portfolio__item--overlay" style={{backgroundColor: "#FF7600"}}></div>
+            </div>
+          </Masonry>
         </div>
       </div>
     </div>
