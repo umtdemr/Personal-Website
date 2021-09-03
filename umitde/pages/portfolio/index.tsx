743d8be3afa17a17d67157d479c1904845ba0d11
umtdemr <umitde692@gmail.com>
2021-09-03 20:19:47 +0300

    added some port

--- umitde/pages/portfolio/index.tsx
+++ umitde/pages/portfolio/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 
@@ -9,8 +10,39 @@ const Portfolio: NextPage = () => {
       <Head>
         <title>Portfolyo - Ümit Demir</title>
       </Head>
-      <div className="portfolio">
-        <h2>Portfolyo</h2>
+      <div className="portfolio_wrapper">
+        <h2 className="portfolio_wrapper--title">Portfolyo</h2>
+        <p className="portfolio_wrapper--text">Bugüne kadar yaptığım bazı işler</p>
+        <div className="portfolio">
+          <div className="portfolio__item">
+            <img src="/images/portfolio/hierapi.png" alt="Hierapi" className="portfolio__item--img"/>
+            <span className="portfolio__item--title">Hierapi</span>
+            <span className="portfolio__item--end">E Ticaret</span>
+            <div className="portfolio__item--overlay"></div>
+            <a href="https://hierapi.com" target="_blank" rel="noreferrer">Projeye göz at</a>
+          </div>
+          <div className="portfolio__item">
+            <img src="/images/portfolio/hierapi.png" alt="Hierapi" className="portfolio__item--img"/>
+            <span className="portfolio__item--title">Hierapi</span>
+            <span className="portfolio__item--end">E Ticaret</span>
+            <div className="portfolio__item--overlay"></div>
+            <a href="https://hierapi.com" target="_blank" rel="noreferrer">Projeye göz at</a>
+          </div>
+          <div className="portfolio__item">
+            <img src="/images/portfolio/hierapi.png" alt="Hierapi" className="portfolio__item--img"/>
+            <span className="portfolio__item--title">Hierapi</span>
+            <span className="portfolio__item--end">E Ticaret</span>
+            <div className="portfolio__item--overlay"></div>
+            <a href="https://hierapi.com" target="_blank" rel="noreferrer">Projeye göz at</a>
+          </div>
+          <div className="portfolio__item">
+            <img src="/images/portfolio/hierapi.png" alt="Hierapi" className="portfolio__item--img"/>
+            <span className="portfolio__item--title">Hierapi</span>
+            <span className="portfolio__item--end">E Ticaret</span>
+            <div className="portfolio__item--overlay"></div>
+            <a href="https://hierapi.com" target="_blank" rel="noreferrer">Projeye göz at</a>
+          </div>
+        </div>
       </div>
     </div>
   )
